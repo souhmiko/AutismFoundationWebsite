680ca170fe70379ed47abbaf008f3b5a7403795a
Penny Lau <pennylau715@gmail.com>
2023-04-20 23:06:46 +0800

    20/4

--- Autism/wwwroot/js/site2.js
+++ Autism/wwwroot/js/site2.js
@@ -1,7 +1,22 @@
-﻿const faqs = document.querySelectorAll(".faq");
+﻿/*const faqs = document.querySelectorAll(".faq");
 
 function toggleActive() {
     this.classList.toggle("active");
 }
 
-faqs.forEach(faq => faq.addEventListener("click", toggleActive));
+faqs.forEach(faq => faq.addEventListener("click", toggleActive));*/
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
