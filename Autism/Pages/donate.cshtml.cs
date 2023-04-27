ac2220703970160f161449f1dfdf9afc0b3b76c1
Penny Lau <pennylau715@gmail.com>
2023-04-27 12:12:00 +0800

    27/4

---
+++ Autism/Pages/donate.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Autism.Pages
+{
+    public class donateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- Autism/wwwroot/js/site.js
+++ Autism/wwwroot/js/site.js
@@ -4,3 +4,8 @@ const navBar = document.querySelector(".nav-bar");
 hamburger.onclick = function () {
     navBar.classList.toggle("active");
 }
+
+
+
+
+
