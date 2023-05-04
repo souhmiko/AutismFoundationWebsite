54638b988f5fb2c1df65ce1d84ac805da99d7d44
Penny Lau <pennylau715@gmail.com>
2023-05-04 20:50:35 +0800

    04/05

---
+++ AdminPanel/wwwroot/js/site.js
@@ -0,0 +1,32 @@
+﻿const body = document.querySelector("body");
+const modetoggle = body.querySelector(".mode-toggle");
+const sidebar = body.querySelector("nav");
+const sidebartoggle = body.querySelector(".sidebar-toggle");
+
+let getMode = localStorage.getItem("mode");
+if (getMode && getMode === "dark") {
+    body.classList.toggle("dark");
+}
+
+let getStatus = localStorage.getItem("status");
+if (getStatus && getStatus === "close") {
+    sidebar.classList.toggle("close");
+}
+
+modetoggle.addEventListener("click", () => {
+    body.classList.toggle("dark");
+    if (body.classList.contains("dark")) {
+        localStorage.setItem("mode", "dark");
+    } else {
+        localStorage.setItem("mode", "light");
+    }
+});
+
+sidebartoggle.addEventListener("click", () => {
+    sidebar.classList.toggle("close");
+    if (sidebar.classList.contains("dark")) {
+        localStorage.setItem("status", "close");
+    } else {
+        localStorage.setItem("status", "open");
+    }
+})
