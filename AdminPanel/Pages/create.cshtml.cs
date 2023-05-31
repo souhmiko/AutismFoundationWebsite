701f94aebf446fab041fdb6523a62d28e4e23c8a
Penny Lau <pennylau715@gmail.com>
2023-06-01 01:13:24 +0800

    1/6

---
+++ AdminPanel/Pages/create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AdminPanel.Pages
+{
+    public class createModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ AdminPanel/Pages/message.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AdminPanel.Pages
+{
+    public class messageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
